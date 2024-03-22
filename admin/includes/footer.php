e48d0690ea8a9fa4422820fe68e5054ea32eed04
“gchandru283” <gchandru283@gmail.com>
2024-03-22 19:39:01 +0530

    Initial Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>Online Voting System</b>
+  </div>
+  <strong> &copy; 2024 Done for <a>Final yr Project</a>.</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+  <div class="container">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2024 Done for<a> Final yr Project</a></strong>
+  </div>
+  <!-- /.container -->
+</footer>
